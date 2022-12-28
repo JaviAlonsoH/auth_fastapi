633dff197816e3b9610605f36dafd7d64fda0fa3
Javi Alonso <javierah@live.com>
2022-12-28 19:46:19 +0100

    Login & signup base added

--- src/web/auth/views.py
+++ src/web/auth/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+from .forms import RegisterForm
 # Create your views here.
 
 
 def index(request):
     return render(request, 'auth/index.html')
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
+
+    return render(request, 'registration/signup.html', {"form": form})
