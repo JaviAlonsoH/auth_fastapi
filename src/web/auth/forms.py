2f7b870ab485be22a02533b938e1fecbe0a41a1e
Javi Alonso <javierah@live.com>
2022-12-29 13:55:14 +0100

    Django Auth finished ftm

--- src/web/auth/forms.py
+++ src/web/auth/forms.py
@@ -5,7 +5,10 @@ from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    username = forms.CharField(required=True, label="Company Name")
+    financial_id_number = forms.CharField(required=True)
 
     class Meta:
         model = User
-        fields = ["username", "email", "password", "password2"]
+        fields = ["financial_id_number", "email",
+                  "username", "password1", "password2"]
